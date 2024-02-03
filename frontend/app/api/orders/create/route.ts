6f38948f4fa6e8300aa628b742914e8fd64ab5de
dpinones <pinonesdamian@gmail.com>
2024-02-03 16:19:35 -0600

    update create order

--- frontend/app/api/orders/create/route.ts
+++ frontend/app/api/orders/create/route.ts
@@ -5,12 +5,14 @@ import { NextResponse } from 'next/server';
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get('userId');
     const amount = searchParams.get('amount');
+    const description = searchParams.get('description');
+    const paymentUrl = searchParams.get('paymentUrl');
     const orderStatus = 'Pending';
     
     try {
         // TODO: validar que exista el usuario
         // if (!user || !ownerName) throw new Error('User not exist');
-        await sql`INSERT INTO Orders (user_id, amount, order_status) VALUES (${userId}, ${amount}, ${orderStatus});`;
+        await sql`INSERT INTO Orders (user_id, amount, description, payment_url, created_at, order_status) VALUES (${userId}, ${amount}, ${description}, ${paymentUrl}, CURRENT_TIMESTAMP, ${orderStatus});`;
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
