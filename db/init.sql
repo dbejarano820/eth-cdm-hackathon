3f67359c36b957b650a7ab3cb41bad7844cb9988
dpinones <pinonesdamian@gmail.com>
2024-02-03 01:38:03 -0600

    update script init

--- db/init.sql
+++ db/init.sql
@@ -3,15 +3,16 @@ CREATE TYPE order_status_enum AS ENUM ('Pending', 'Completed', 'Incomplete', 'Ex
 
 -- Users Table
 CREATE TABLE Users (
-    id VARCHAR(255) SERIAL PRIMARY KEY,
-    correo VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     total_balance INTEGER NOT NULL
 );
 
 -- Wallets Table
 CREATE TABLE Wallets (
-    id VARCHAR(255) SERIAL PRIMARY KEY,
-    user_id VARCHAR(255) REFERENCES Users(id),
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users(id),
     public_key VARCHAR(255) NOT NULL,
     private_key VARCHAR(255) NOT NULL,
     blockchain VARCHAR(255) NOT NULL,
@@ -20,10 +21,10 @@ CREATE TABLE Wallets (
 
 -- Orders Table
 CREATE TABLE Orders (
-    id VARCHAR(255) SERIAL PRIMARY KEY,
-    user_id VARCHAR(255) REFERENCES Users(id),
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES Users(id),
     amount INTEGER NOT NULL,
     order_status order_status_enum NOT NULL,
-    transaction_hash VARCHAR(255) NOT NULL,
-    payed_amount INTEGER NOT NULL
+    transaction_hash VARCHAR(255),
+    payed_amount INTEGER
 );
