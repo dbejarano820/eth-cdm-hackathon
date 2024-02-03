38a06bf34c26961223c73a18b453a02a0f333b75
Daniel Bejarano <dbejarano820@gmail.com>
2024-02-03 15:44:35 -0600

    prettier format

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -3,8 +3,8 @@ import { Title, Text } from '@tremor/react';
 export default function Loading() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <div className='flex justify-around'>
-<Text>Loading...</Text>
+      <div className="flex justify-around">
+        <Text>Loading...</Text>
       </div>
     </main>
   );
--- frontend/app/order/page.tsx
+++ frontend/app/order/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import React, { useState } from 'react';
-import { TextInput, Metric, Text, Title, NumberInput, Button } from '@tremor/react';
+import {
+  TextInput,
+  Metric,
+  Text,
+  Title,
+  NumberInput,
+  Button
+} from '@tremor/react';
 import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
 import { SelectChain } from '../components/select-chain';
 import { BackButton } from '../components/back-button';
-import QRCode from 'qrcode.react'; 
+import QRCode from 'qrcode.react';
 
 export default function StatsPage() {
   const [selectedChain, setSelectedChain] = useState<string>('');
@@ -28,31 +35,37 @@ export default function StatsPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <BackButton />
-      <div className='my-6'>
-        <Metric className='mb-3'>Genere un cobro</Metric>
-        <SelectChain value={selectedChain} setValue={setSelectedChain} disabled={isSubmitting} />
-        <div className='my-3'>
+      <div className="my-6">
+        <Metric className="mb-3">Genere un cobro</Metric>
+        <SelectChain
+          value={selectedChain}
+          setValue={setSelectedChain}
+          disabled={isSubmitting}
+        />
+        <div className="my-3">
           <Title>Monto por cobrar:</Title>
-          <NumberInput 
-            icon={CurrencyDollarIcon} 
-            placeholder="Monto..." 
-            value={amount} 
+          <NumberInput
+            icon={CurrencyDollarIcon}
+            placeholder="Monto..."
+            value={amount}
             onChange={(e) => setAmount(e.target.value)}
             disabled={isSubmitting}
           />
         </div>
-        <TextInput 
-          className='h-8' 
-          placeholder="Ingrese una descripción" 
-          type="text" 
+        <TextInput
+          className="h-8"
+          placeholder="Ingrese una descripción"
+          type="text"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           disabled={isSubmitting}
         />
       </div>
-      <Button onClick={handleSubmit} disabled={isSubmitting}>Cobrar</Button>
+      <Button onClick={handleSubmit} disabled={isSubmitting}>
+        Cobrar
+      </Button>
       {qrCode && !isSubmitting && (
-        <div className='my-3'>
+        <div className="my-3">
           <QRCode value={qrCode} />
         </div>
       )}
