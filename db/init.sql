f6edd2d8f21f445033925ed63d9731c567155582
Daniel Bejarano <dbejarano820@gmail.com>
2024-02-03 01:25:39 -0600

    auto increment ids in db

--- db/init.sql
+++ db/init.sql
@@ -3,14 +3,14 @@ CREATE TYPE order_status_enum AS ENUM ('Pending', 'Completed', 'Incomplete', 'Ex
 
 -- Users Table
 CREATE TABLE Users (
-    id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) SERIAL PRIMARY KEY,
     correo VARCHAR(255) NOT NULL,
     total_balance INTEGER NOT NULL
 );
 
 -- Wallets Table
 CREATE TABLE Wallets (
-    id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) SERIAL PRIMARY KEY,
     user_id VARCHAR(255) REFERENCES Users(id),
     public_key VARCHAR(255) NOT NULL,
     private_key VARCHAR(255) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE Wallets (
 
 -- Orders Table
 CREATE TABLE Orders (
-    id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) SERIAL PRIMARY KEY,
     user_id VARCHAR(255) REFERENCES Users(id),
     amount INTEGER NOT NULL,
     order_status order_status_enum NOT NULL,
