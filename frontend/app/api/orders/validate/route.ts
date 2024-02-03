90dc509dc86381aabb55588da4c1b6588e31ddc3
dpinones <pinonesdamian@gmail.com>
2024-02-03 16:19:09 -0600

    wip validate order

--- frontend/app/api/orders/validate/route.ts
+++ frontend/app/api/orders/validate/route.ts
@@ -2,7 +2,7 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 import { Order } from '../../../interfaces';
-
+import { ethers, Wallet } from "ethers";
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
@@ -39,10 +39,387 @@ export async function GET(request: Request) {
         await sql`UPDATE Orders SET transaction_hash = ${tx}, payed_amount = ${newBalance}, order_status = ${status} WHERE id = ${orderId};`;
 
         // pegarle al contrato escrow
-        
+        const ABI = [
+            {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+            },
+            {
+            "inputs": [],
+            "name": "InvalidInitialization",
+            "type": "error"
+            },
+            {
+            "inputs": [],
+            "name": "NotInitializing",
+            "type": "error"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+                }
+            ],
+            "name": "OwnableInvalidOwner",
+            "type": "error"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+                }
+            ],
+            "name": "OwnableUnauthorizedAccount",
+            "type": "error"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": true,
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+                },
+                {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+                },
+                {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+                }
+            ],
+            "name": "CollateralDeposit",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": false,
+                "internalType": "uint64",
+                "name": "version",
+                "type": "uint64"
+                }
+            ],
+            "name": "Initialized",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": true,
+                "internalType": "uint256",
+                "name": "orderId",
+                "type": "uint256"
+                },
+                {
+                "components": [
+                    {
+                    "internalType": "address",
+                    "name": "_merchant",
+                    "type": "address"
+                    },
+                    {
+                    "internalType": "uint256",
+                    "name": "_amount",
+                    "type": "uint256"
+                    },
+                    {
+                    "internalType": "string",
+                    "name": "_status",
+                    "type": "string"
+                    },
+                    {
+                    "internalType": "uint256",
+                    "name": "_tx",
+                    "type": "uint256"
+                    }
+                ],
+                "indexed": false,
+                "internalType": "struct Escrow.Order",
+                "name": "order",
+                "type": "tuple"
+                }
+            ],
+            "name": "OrderUpdate",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+                },
+                {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+                }
+            ],
+            "name": "OwnershipTransferred",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": false,
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+                },
+                {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+                }
+            ],
+            "name": "Received",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": false,
+                "internalType": "address",
+                "name": "client",
+                "type": "address"
+                },
+                {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+                }
+            ],
+            "name": "Withdraw",
+            "type": "event"
+            },
+            {
+            "anonymous": false,
+            "inputs": [
+                {
+                "indexed": true,
+                "internalType": "address",
+                "name": "receiver",
+                "type": "address"
+                },
+                {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+                }
+            ],
+            "name": "Withdrawal",
+            "type": "event"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "tokenOwner",
+                "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                "internalType": "uint256",
+                "name": "balance",
+                "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "uint256",
+                "name": "orderId",
+                "type": "uint256"
+                }
+            ],
+            "name": "getOrder",
+            "outputs": [
+                {
+                "components": [
+                    {
+                    "internalType": "address",
+                    "name": "_merchant",
+                    "type": "address"
+                    },
+                    {
+                    "internalType": "uint256",
+                    "name": "_amount",
+                    "type": "uint256"
+                    },
+                    {
+                    "internalType": "string",
+                    "name": "_status",
+                    "type": "string"
+                    },
+                    {
+                    "internalType": "uint256",
+                    "name": "_tx",
+                    "type": "uint256"
+                    }
+                ],
+                "internalType": "struct Escrow.Order",
+                "name": "",
+                "type": "tuple"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+            },
+            {
+            "inputs": [],
+            "name": "getOwner",
+            "outputs": [
+                {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "nativeToken",
+                "type": "address"
+                },
+                {
+                "internalType": "address",
+                "name": "aavePoolProxy",
+                "type": "address"
+                }
+            ],
+            "name": "initialize",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+            },
+            {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+            },
+            {
+            "inputs": [],
+            "name": "renounceOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+                }
+            ],
+            "name": "transferOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "merchant",
+                "type": "address"
+                },
+                {
+                "internalType": "uint256",
+                "name": "orderId",
+                "type": "uint256"
+                },
+                {
+                "internalType": "uint256",
+                "name": "transaction",
+                "type": "uint256"
+                },
+                {
+                "internalType": "uint256",
+                "name": "orderAmount",
+                "type": "uint256"
+                },
+                {
+                "internalType": "string",
+                "name": "orderStatus",
+                "type": "string"
+                }
+            ],
+            "name": "validate",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+            },
+            {
+            "inputs": [
+                {
+                "internalType": "address",
+                "name": "merchant",
+                "type": "address"
+                }
+            ],
+            "name": "withdraw",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+            },
+            {
+            "stateMutability": "payable",
+            "type": "receive"
+            }
+        ];
 
+        const provider = new ethers.providers.WebSocketProvider("wss://api.avax-test.network/ext/bc/C/ws");
+
+        const wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
+        const contract = new ethers.Contract(process.env.ESCROW_CONTRACT!, ABI, provider);
+        
+        const contractWithWallet = contract.connect(wallet);
+        // TODO: update user wallet
+        const resultTx = await contractWithWallet.validate('0xEBdf70B26e5e7520B8B79e1D01eD832f48972B09', orderId, tx, order.amount, status);
+        await resultTx.wait();
 
     } catch (error) {
+        console.log("error: ", error);
         return NextResponse.json({ error }, { status: 500 });
     }
     return NextResponse.json({ status: 200 });
