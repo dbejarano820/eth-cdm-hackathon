6b8ba7c3561516c2ef3004a02d6d3f0d10243cb3
dpinones <pinonesdamian@gmail.com>
2024-02-03 19:05:09 -0600

    update create User

--- frontend/app/api/users/create/route.ts
+++ frontend/app/api/users/create/route.ts
@@ -9,10 +9,15 @@ export async function GET(request: Request) {
 
     try {
         const users_result = await sql`SELECT * from Users WHERE email = ${email};`;
-        if (users_result.rows.length > 0) throw new Error('Error: User exist');
+        if (users_result.rows.length > 0) {
+            const user = users_result.rows[0];
+            return NextResponse.json({ user }, { status: 200 });
+        }
         await sql`INSERT INTO Users (name, email, total_balance) VALUES (${name}, ${email}, ${totalBalance});`;
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
-    return NextResponse.json({ status: 200 });
+    const users_result = await sql`SELECT * from Users WHERE email = ${email};`;
+    const user = users_result.rows[0];
+    return NextResponse.json({ user }, { status: 200 });
 }
