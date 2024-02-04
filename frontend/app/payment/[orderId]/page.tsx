e8d79287a2e5534126ab0c518d5f8abcaaf5c07a
dpinones <pinonesdamian@gmail.com>
2024-02-04 00:26:52 -0600

    add order status

--- frontend/app/payment/[orderId]/page.tsx
+++ frontend/app/payment/[orderId]/page.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { sql } from '@vercel/postgres';
-import { ESCROW_MAP } from '../../constants';
+// import { ESCROW_MAP } from '../../constants';
 import CopyToClipboardButton from '../../components/copy-text';
 import TransactionProof from '../../components/transaction-proof';
 import { Metric, Text } from '@tremor/react';
@@ -18,13 +18,15 @@ export default async function PaymentPage({
     `;
     
   const data = results.rows[0];
-  const escrow_address = ESCROW_MAP['Avalanche'];
+  const escrow_address = process.env.ESCROW_CONTRACT!;
+  // const escrow_address = ESCROW_MAP['Avalanche'];
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <div className="flex flex-col items-center gap-5">
         <Metric>Pago para {data?.name || 'Tienda X'}</Metric>
         <Text>Monto: {data?.amount}</Text>
+        <Text>Estado: {data?.order_status}</Text>
         <CopyToClipboardButton textToCopy={escrow_address} />
         <div className="flex flex-col items-center my-6">
           <TransactionProof orderId={params.orderId} />
